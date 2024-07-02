9c70481309f0d09ccb3b663c7353e6eacbbef1b7
Lucy-xDeer <1185802007@qq.com>
2024-07-02 22:04:15 +0800

    后端寻回

---
+++ src/main/java/org/intership/clubmate/entity/Club.java
@@ -0,0 +1,22 @@
+package org.intership.clubmate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Club {
+    private Integer id;
+    private String name;
+    private String introduce;
+    private Integer createUserId;
+    private String avatarUrl;
+    private Integer status;
+    private LocalDateTime registerTime;
+    private Integer totalNumber;
+    private Integer tags;
+}
---
+++ src/main/java/org/intership/clubmate/service/ClubService.java
@@ -0,0 +1,13 @@
+package org.intership.clubmate.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.intership.clubmate.entity.Club;
+
+public interface ClubService extends IService<Club> {
+    void insertClub(Club club);
+    void deleteById(Integer id);
+    Club getById(Integer id);
+    IPage<Club> list(int pageNo,int pageSize);
+    void update(Club club);
+}
