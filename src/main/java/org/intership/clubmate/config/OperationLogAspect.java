57522b6a6dc462e0f5a528ad34b871da072974af
quee <2205939061@qq.com>
2024-07-08 11:20:19 +0800

    操作日志的修改

--- src/main/java/org/intership/clubmate/config/OperationLogAspect.java
+++ src/main/java/org/intership/clubmate/config/OperationLogAspect.java
@@ -2,6 +2,8 @@ package org.intership.clubmate.config;
 
 
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
@@ -9,8 +11,10 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.intership.clubmate.entity.BaseLog;
 import org.intership.clubmate.entity.ErrorLog;
 import org.intership.clubmate.entity.OperaLog;
+import org.intership.clubmate.pojo.ResponseResult;
 import org.intership.clubmate.service.ErrorLogService;
 import org.intership.clubmate.service.OperaLogService;
 import org.springframework.stereotype.Component;
@@ -19,8 +23,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 
 import java.lang.reflect.Method;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Component
 @Aspect
@@ -36,20 +39,19 @@ public class OperationLogAspect {
 
     }
 
-    @Pointcut("@annotation(org.intership.clubmate.config.Log)")
-    public void errorLogPointCut() {
-    }
 
     @AfterReturning(value = "operaLogPointCut()", returning = "keys")
-    public void saveOperationLog(JoinPoint joinPoint, Object keys) {
+    public void saveOperationLog(JoinPoint joinPoint, ResponseResult keys) {
         //获取RequestAttributes
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         //从获取到的RequestAttributes中获取HttpServletRequest的信息
         HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
 
-        OperaLog operaLog = new OperaLog();
+
+        BaseLog baseLog =getLog(keys.getCode()) ;
 
         try {
+
             MethodSignature signature = (MethodSignature) joinPoint.getSignature();
             //获取织入点的方法
             Method method = signature.getMethod();
@@ -58,88 +60,62 @@ public class OperationLogAspect {
             if (log != null) {
                 String operaModule = log.operaModule();
                 String operaType = log.operaType();
-                operaLog.setOperaModule(operaModule);
-                operaLog.setOperaType(operaType);
+                baseLog.setOperaModule(operaModule);
+                baseLog.setOperaType(operaType);
             }
             //获取请求的类名
             String className = joinPoint.getTarget().getClass().getName();
-            //获取请求的方法
-            String methodName = method.getName();
-            methodName = className + "." + methodName;
 
-            Map<String, String> rtnMap = converMap(request.getParameterMap());
-            //将参数所在的数组转为json
-            String params = JSON.toJSONString(rtnMap);
 
+
+            // 获取方法的参数
+            Object[] args = joinPoint.getArgs();
+            ObjectMapper objectMapper = new ObjectMapper();
+            String params = "";
+            try {
+                params =  objectMapper.writeValueAsString(args);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+
+
+            baseLog.setId(null);
             //请求参数
-            operaLog.setOperateRequest(params);
+            baseLog.setOperaRequest(params);
             //返回结果
-            operaLog.setOperateResponse(JSON.toJSONString(keys));
+
             //操作员ip地址
-            operaLog.setIp(request.getRemoteAddr());
+            baseLog.setIp(request.getRemoteAddr());
             //请求URI
-            operaLog.setOperaUrl(request.getRequestURI());
+            baseLog.setOperaUrl(request.getRequestURI());
             //创建时间（操作时间）
-            operaLog.setOperaTime(new Date());
-            operaLog.setId(1);
-            operaLogService.add(operaLog);
+            baseLog.setOperaTime(new Date());
+            baseLog.setUserId(1);
+            if(keys.getCode()==200){
+                OperaLog operaLog =(OperaLog) baseLog;
+                operaLog.setOperaResponse(JSON.toJSONString(keys));
+                operaLogService.add(operaLog);
+            }
+            else {
+                ErrorLog errorLog =(ErrorLog) baseLog;
+                errorLog.setErrCode(keys.getCode());
+                errorLog.setErrMessage(keys.getMsg());
+                errorLogService.add(errorLog);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
 
-    /*@AfterReturning(value = "errorLogPointCut()", returning = "a")
-    public void saveErrorLog(JoinPoint joinPoint, Object a){
-        //获取RequestAttributes
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        //从RequestAttributes中获取HttpServletRequest的信息
-        HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
 
-        ErrorLog errorLog = new ErrorLog();
-        try {
-            //在切面织入点通过反射机制获取织入点的方法
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-            //获取织入点的方法
-            Method method = signature.getMethod();
-            //获取操作
-            Log log = method.getAnnotation(Log.class);
-            //获取请求的类名
-            String className = joinPoint.getTarget().getClass().getName();
-            //获取请求的方法
-            String methodName = method.getName();
-            methodName = className + "." + methodName;
-            //请求的参数
-            Map<String, String> rtnMap = converMap(request.getParameterMap());
-            String params = JSON.toJSONString(rtnMap);
-            //请求方法名
-            errorLog.setOperaMethod(methodName);
-            //异常信息
-            errorLog.setErrMessage(JSON.toJSONString(a));
-            //请求URI
-            errorLog.setOperaUrl(request.getRequestURI());
-            //操作员ip地址
-            errorLog.setOperaIp(request.getRemoteAddr());
-            //发生异常的时间
-            errorLog.setOperaTime(new Date());
-            errorLog.setUserId(1);
-
-            errorLogService.save(errorLog);
-        }catch (Exception exception){
-            exception.printStackTrace();
+    public BaseLog getLog(int code){
+        if(code==200) {
+           return new OperaLog();}
+        else {
+            return new ErrorLog();
         }
-    }*/
-
-
-    public Map<String, String> converMap(Map<String,String[]> paramMap){
-        Map<String, String> rtnMap = new HashMap<>();
-        for (String key : paramMap.keySet()) {
-            rtnMap.put(key,paramMap.get(key)[0]);
-        }
-        return rtnMap;
     }
-
-
 
 }
 
--- src/main/java/org/intership/clubmate/entity/OperaLog.java
+++ src/main/java/org/intership/clubmate/entity/OperaLog.java
@@ -5,31 +5,18 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@TableName("login_log")
-public class OperaLog {
-    @TableId(value="id",type = IdType.AUTO)
-    private int id;
-    @TableField("user_id")
-    private int userId;
-    @TableField("ip")
-    private String ip;
-    @TableField("opera_time")
-    private Date operaTime;
-    @TableField("opera_request")
-    private String operateRequest;
+@Data
+@TableName("opera_log")
+public class OperaLog extends BaseLog{
+
     @TableField("opera_response")
-    private String operateResponse;
-    @TableField("opera_type")
-    private String operaType;
-    @TableField("opera_url")
-    private String operaUrl;
-    @TableField("opera_module")
-    private String operaModule;
+    private String operaResponse;
+
+
 }
