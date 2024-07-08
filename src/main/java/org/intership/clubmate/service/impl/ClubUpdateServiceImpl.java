b346e954abbab87e43bd8c30d1806ca9c2de48ed
Lucy-xDeer <1185802007@qq.com>
2024-07-08 10:16:17 +0800

    社团更新

---
+++ src/main/java/org/intership/clubmate/service/impl/ClubUpdateServiceImpl.java
@@ -0,0 +1,45 @@
+package org.intership.clubmate.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.ibatis.annotations.Insert;
+import org.intership.clubmate.entity.Club;
+import org.intership.clubmate.entity.ClubUpdate;
+import org.intership.clubmate.mapper.ClubMapper;
+import org.intership.clubmate.mapper.ClubUpdateMapper;
+import org.intership.clubmate.service.ClubUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClubUpdateServiceImpl extends ServiceImpl<ClubUpdateMapper, ClubUpdate> implements ClubUpdateService {
+
+    @Autowired
+    private ClubUpdateMapper clubUpdateMapper;
+    @Autowired
+    private ClubMapper clubMapper;
+    @Override
+    public void insertUpdate(ClubUpdate clubUpdate) {
+        clubUpdateMapper.insert(clubUpdate);
+    }
+
+    @Override
+    public void cancelUpdate(Integer id) {
+        clubUpdateMapper.deleteById(id);
+    }
+
+    @Override
+    public Club getClubById(Integer id) {
+        Club club=new Club();
+        ClubUpdate clubUpdate=clubUpdateMapper.selectById(id);
+        club=clubMapper.selectById(id);
+        //重置可能更新的属性
+        club.setId(clubUpdate.getId());
+        club.setName(clubUpdate.getName());
+        club.setIntroduce(clubUpdate.getIntroduce());
+        club.setCollage(clubUpdate.getCollage());
+        club.setCreateUserId(clubUpdate.getCreateUserId());
+
+        return club;
+    }
+}
