4626feefba0c849ef6b5ced6a071663575ee8b33
Jtyyhhxsxa <137083067+Jtyyhhxsxa@users.noreply.github.com>
2024-07-10 11:07:37 +0800

    配置全局身份变量

--- src/stores/web.js
+++ src/stores/web.js
@@ -3,7 +3,8 @@ import { reactive } from "vue";
 
 export const useWebStore = defineStore('web', () => {
   const web = reactive({
-    status: false,
+    status: false,//登录状态
+    identity: ''//身份
   })
 
   return {
