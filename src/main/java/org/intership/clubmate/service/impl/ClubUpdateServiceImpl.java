6b8d34a1bd9189e0e5ddf943e13b8daa91fe8b91
Lucy-xDeer <1185802007@qq.com>
2024-07-11 08:39:41 +0800

    gpt初版+更新列表接口

--- src/main/java/org/intership/clubmate/service/impl/ClubUpdateServiceImpl.java
+++ src/main/java/org/intership/clubmate/service/impl/ClubUpdateServiceImpl.java
@@ -1,6 +1,9 @@
 package org.intership.clubmate.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.ibatis.annotations.Insert;
 import org.intership.clubmate.entity.Club;
@@ -41,5 +44,12 @@ public class ClubUpdateServiceImpl extends ServiceImpl<ClubUpdateMapper, ClubUpd
         club.setCreateUserId(clubUpdate.getCreateUserId());
 
         return club;
+    }
+
+    @Override
+    public IPage<ClubUpdate> getUpdate() {
+
+        Page<ClubUpdate> page=new Page<>();
+        return clubUpdateMapper.selectPage(page,null);
     }
 }
